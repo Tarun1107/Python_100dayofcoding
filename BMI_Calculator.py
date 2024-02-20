1c183fe087b571b2870db4cad7c917e6fdeb07b0
Varanasi Prasanna Tarun Teja <82260072+Tarun1107@users.noreply.github.com>
2024-02-20 12:46:52 -0500

    BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -7,5 +7,15 @@ height = float(input("Enter your height in meters(m): \n"))
 weight = int(input("Enter your weight in Kgs: \n"))
 
 #BMI calculation
-BMI = int(weight/(height**2))
+BMI = weight/(height**2)
 print(BMI)
+
+#Generates output as per the BMI
+if BMI < 18.5:
+    print(f"You are Underweight as your BMI is {BMI}")
+elif BMI  >= 18.5 and BMI <= 24.9:
+    print(f"Your weight is healthy as your BMI is {BMI}")
+elif BMI > 25.0 and BMI <= 29.9:
+    print(f"You are Overweight as you BMI is {BMI}")
+else:
+    print(f"You are Obese as your BMI is {BMI}")
