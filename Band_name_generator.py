cccd84477243057e17a02be94360e051659239c8
Varanasi Prasanna Tarun Teja <82260072+Tarun1107@users.noreply.github.com>
2024-02-20 11:32:33 -0500

    Day-1: Band name generator
    For Day-1 I have created a code to generate a band name based on the inputs provided by the user.

---
+++ Band_name_generator.py
@@ -0,0 +1,10 @@
+#Day-1 Project: Band Name Generator
+#Printing welcome line to generate a band name
+print("Welcome to the Band Name Generator")
+
+#Getting inputs from user
+city = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+#Printing out band name
+print(f'Your band name could be  "{city} {pet_name}"')
